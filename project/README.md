a79cb3f3dce3e6e3f449c658e7b7c731f62cd426
szwc <491157109@qq.com>
2017-08-07 15:36:52 +0800

    熟悉了node操作mysql

---
+++ project/README.md
@@ -0,0 +1,16 @@
+# a simple project for node
+
+## app.js 
+
+
+## server.js
+
+实现http服务
+
+## router.js
+
+处理来自不同路由的请求
+
+## req_handle.js
+
+不同路由的处理事件
---
+++ project/mysql.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql');
+// var connection = mysql.createConnection({
+// 	host:'127.0.0.1',
+// 	port:'3306',
+// 	user:'root',
+// 	password:'hongju0908',
+// 	database:'test'
+// })
+
+// connection.connect();
+// var querySql ='SELECT * FROM websites WHERE id=1';
+// connection.query(querySql,function (err,result) {
+// 	if(err){
+// 		console.log('[SELECT ERROR - ]',err.message);
+// 		return;
+// 	}
+// 	console.log(result);
+// })
+// connection.end();
+exports.m = mysql;
+console.log(module);
